645c71bd8f636e8d2622083aef750fc5b2454995
Kibret Woldemichael <kwoldemichael@sdsu.edu>
2020-10-11 13:28:39 -0700

    Coin flip complete

---
+++ js/02-coinflip.js
@@ -0,0 +1,30 @@
+var coinFlip = Math.random;
+var valid = false;
+var choice;
+do {
+    choice = window.prompt("Heads(H) or Tails(T)").toUpperCase();
+    if ((choice != "H") && (choice != "T")) {
+        window.alert("Invalid choice: " + choice);
+    } else {
+        valid = true;
+    }
+} while (valid == false);
+
+var result = "";
+if (coinFlip >= 0.5) {
+    result += "The flip was tails";
+    if (choice == "H") {
+        result += " and you chose heads... you lose!";
+    } else {
+        result += " and you chose tails... you win!";
+    }
+} else {
+    result += "The flip was heads";
+    if (choice == "H") {
+        result += " and you chose heads... you win!";
+    } else {
+        result += " and you chose tails... you lose!";
+    }
+}
+
+window.alert(result);
